b9919a23d54f8533044e091d3ab6a79df436280a
LOO ZHENG YUAN <loozhengyuan@ZHENGYUNMacMini.cck.zhengyuan.me>
2018-11-26 21:40:39 +0800

    added new relational models

--- web/views.py
+++ web/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.db.models import Count
-from .models import Entries
+from .models import HostUni, HostCourse, NTUCourse, CourseMatch, Entries
 
 # Create your views here.
 def index(request):
@@ -10,5 +10,6 @@ def index(request):
 def results(request):
     if request.method == 'POST':
         queried = [x.strip() for x in request.POST['courses'].split(",")]
-        return render(request, 'web/base_results.html')
+        matches = CourseMatch.objects.filter(ntu_course__code__in=queried).annotate(total_clearable=Count('ntu_course__code')).filter(total_clearable__gte=2)
+        return render(request, 'web/base_results.html', {'matches': matches})
     return redirect(index)
