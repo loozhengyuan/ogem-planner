a327852c43521ba42707b59648415a1f41e1a933
LOO ZHENG YUAN <loozhengyuan@ZHENGYUNMacMini.cck.zhengyuan.me>
2018-11-26 20:45:56 +0800

    create results view

--- web/views.py
+++ web/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db.models import Count
 from .models import Entries
 
 # Create your views here.
 def index(request):
     entries = Entries.objects.all()
     return render(request, 'web/base_index.html', {'entries': entries})
+
+def results(request):
+    if request.method == 'POST':
+        queried = [x.strip() for x in request.POST['courses'].split(",")]
+        return render(request, 'web/base_results.html')
+    return redirect(index)
