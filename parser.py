4e15457b1f30d2f337b30f785437505c12800249
LOO ZHENG YUAN <loozhengyuan@ZHENGYUNMacMini.cck.zhengyuan.me>
2018-11-27 00:46:16 +0800

    delete old prototype files; converted to django mgmt commands

--- parser.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('output.csv')
-courses = df[['ntu_course_code', 'ntu_course_title']].drop_duplicates(subset='ntu_course_code').sort_values(by='ntu_course_code').reset_index(drop=True)
-hosts = df[['host_uni']].drop_duplicates(subset='host_uni').sort_values(by='host_uni').reset_index(drop=True)
-
-print(courses)
-print(hosts)
