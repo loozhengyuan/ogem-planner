1922160c4fb92bed069d5357e208f575743e95a0
LOO ZHENG YUAN <loozhengyuan@ZHENGYUNMacMini.cck.zhengyuan.me>
2018-11-27 00:48:38 +0800

    remove deprecated Entries model

--- web/models.py
+++ web/models.py
@@ -20,15 +20,3 @@ class CourseMatch(models.Model):
     status = models.CharField(max_length=255)
     last_updated = models.CharField(max_length=255)
     validity = models.CharField(max_length=255)
-
-
-class Entries(models.Model):
-    host_uni = models.CharField(max_length=255)
-    ntu_course_code = models.CharField(max_length=255)
-    ntu_course_title = models.CharField(max_length=255)
-    host_course_code = models.CharField(max_length=255)
-    host_course_title = models.CharField(max_length=255)
-    sem_last_offered = models.CharField(max_length=255)
-    status = models.CharField(max_length=255)
-    last_updated = models.CharField(max_length=255)
-    validity = models.CharField(max_length=255)
