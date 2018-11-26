f72f613b3cf820085d6c719c3ec6d9ea002ab647
LOO ZHENG YUAN <loozhengyuan@ZHENGYUNMacMini.cck.zhengyuan.me>
2018-11-27 00:36:36 +0800

    add about and hostuni views

--- web/views.py
+++ web/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Count
 from .models import HostUni, HostCourse, NTUCourse, CourseMatch, Entries
 
 # Create your views here.
 def index(request):
-    entries = Entries.objects.all()
-    return render(request, 'web/base_index.html', {'entries': entries})
+    return render(request, 'web/base_index.html')
+
+
+def about(request):
+    return render(request, 'web/base_about.html')
+
 
 def results(request):
     if request.method == 'POST':
@@ -13,3 +17,13 @@ def results(request):
         matches = CourseMatch.objects.filter(ntu_course__code__in=queried).annotate(total_clearable=Count('ntu_course__code')).filter(total_clearable__gte=2)
         return render(request, 'web/base_results.html', {'matches': matches})
     return redirect(index)
+
+
+def hostuni(request, hostuni_id):
+    if request.method == 'POST':
+        pass
+    host_uni = get_object_or_404(HostUni, pk=hostuni_id)
+    matches = CourseMatch.objects.filter(host_uni=host_uni)
+    return render(request, 'web/base_hostuni.html', {'host_uni': host_uni,
+                                                        'matches': matches})
+
