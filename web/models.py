91cbb123a73cbbbaa683bdd42c397e65d8090d80
LOO ZHENG YUAN <loozhengyuan@ZHENGYUNMacMini.cck.zhengyuan.me>
2018-11-26 15:11:57 +0800

    create db model for scraped data

--- web/models.py
+++ web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Entries(models.Model):
+    host_uni = models.CharField(max_length=255)
+    ntu_course_code = models.CharField(max_length=255)
+    ntu_course_title = models.CharField(max_length=255)
+    host_course_code = models.CharField(max_length=255)
+    host_course_title = models.CharField(max_length=255)
+    sem_last_offered = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    last_updated = models.CharField(max_length=255)
+    validity = models.CharField(max_length=255)
