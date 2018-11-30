061168b9a9309b99d8e3916bf7a6cc53d2a571f9
LOO ZHENG YUAN <loozhengyuan@ZHENGYUAN-Mini.cck.zhengyuan.me>
2018-12-01 00:56:10 +0800

    set uuid, created, modified fields on all tables

--- web/models.py
+++ web/models.py
@@ -1,18 +1,27 @@
+import uuid
 from django.db import models
 
+class Base(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+
 # Create your models here.
-class HostUni(models.Model):
+class HostUni(Base):
     name = models.CharField(max_length=255)
 
-class NTUCourse(models.Model):
+class NTUCourse(Base):
     code = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
 
-class HostCourse(models.Model):
+class HostCourse(Base):
     code = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
 
-class CourseMatch(models.Model):
+class CourseMatch(Base):
     host_uni = models.ForeignKey(HostUni, on_delete=models.CASCADE)
     ntu_course = models.ForeignKey(NTUCourse, on_delete=models.CASCADE)
     host_course = models.ForeignKey(HostCourse, on_delete=models.CASCADE)
